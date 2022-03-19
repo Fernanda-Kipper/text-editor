b831671a8674c4bfcd900be598da49cba415fceb
Fernanda <nanda.kipper@gmail.com>
2022-03-19 15:01:24 -0300

    feat: add loading component

---
+++ src/components/loading.tsx
@@ -0,0 +1,74 @@
+import { motion, MotionStyle } from "framer-motion";
+
+const loadingContainerVariants = {
+  start: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+}
+
+const loadingCircleTransition = {
+  duration: 0.5,
+  yoyo: Infinity,
+  ease: "easeInOut",
+}
+
+const loadingCircleVariants = {
+  start: {
+    y: "0%",
+  },
+  end: {
+    y: "100%",
+  },
+}
+
+const loadingContainer: MotionStyle = {
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+
+const loadingCircle:MotionStyle = {
+  backgroundColor: '#AEC0FF',
+  width: '24px',
+  height: '24px',
+  borderRadius: '24px',
+  display:'inline-block',
+  margin: '0 8px'
+}
+
+export function Loading(){
+  return(
+    <div>
+      <motion.div
+        style={loadingContainer}
+        variants={loadingContainerVariants}
+        initial="start"
+        animate="end"
+      >
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+        <motion.span
+          style={loadingCircle}
+          variants={loadingCircleVariants}
+          transition={loadingCircleTransition}
+        />
+      </motion.div>
+    </div>
+  )
+}
